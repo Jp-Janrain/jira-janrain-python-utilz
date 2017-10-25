ded1b0d57ea4f3910db671c0289bbc38d61d4280
Jp-Janrain <jrowan@janrain.com>
2017-10-25 15:27:56 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='JIRA/Janrain Utilities',
+    version='0.1dev',
+    packages=['jira-janrain',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
